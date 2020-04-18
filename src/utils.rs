43907452f80fd5e65b2e98b2c1805722fcf6fd72
Jeriel <jeriel.verissimo@protonmail.com>
2020-04-18 02:41:50 -0300

    feat: load window from glade source
    Refs: #2

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,7 @@
 macro_rules! get_widget {
     ($builder:expr, $wtype:ty, $name:ident) => {
         let $name: $wtype = $builder
-            .get_object(stringify!(name))
+            .get_object(stringify!($name))
             .expect(&format!("Could not find widget \"{}\"", stringify!($name)));
     };
 }
--- src/window.rs
+++ src/window.rs
@@ -5,20 +5,17 @@ use crate::config::APP_ID;
 pub struct Window {
     pub widget: gtk::ApplicationWindow,
     builder: gtk::Builder,
-    settings: gio::Settings,
 }
 
 impl Window {
     pub fn new() -> Self {
-        let settings = gio::Settings::new(APP_ID);
-
-        let builder = gtk::Builder::new();
+        let glade_src = include_str!("../data/resources/ui/window.ui");
+        let builder = gtk::Builder::new_from_string(glade_src);
         get_widget!(builder, gtk::ApplicationWindow, window);
 
-        let mut window_widget = Window {
+        let window_widget = Window {
             widget: window,
             builder,
-            settings,
         };
 
         //window_widget.load_widgets();
