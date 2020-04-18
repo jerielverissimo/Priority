edb4a2df25f348e2e8342ada1a2b22d8bdb0bd9f
Jeriel <jeriel.verissimo@protonmail.com>
2020-04-18 01:43:27 -0300

    feat: add quit action
    Refs: #2

--- src/application.rs
+++ src/application.rs
@@ -5,20 +5,33 @@ use std::env;
 
 use crate::config;
 
-pub(crate) struct Application {
+pub struct Application {
     app: gtk::Application,
 }
 
 impl Application {
-    pub(crate) fn new() -> Self {
+    pub fn new() -> Self {
         let app = gtk::Application::new(Some(config::APP_ID), gio::ApplicationFlags::FLAGS_NONE)
             .expect("Application::new failed");
 
         let application = Self { app };
 
         application.setup_signals();
+        application.setup_gactions();
 
         application
+    }
+
+    fn setup_gactions(&self) {
+        // Quit
+        action!(
+            self.app,
+            "quit",
+            clone!(@strong self.app as app => move |_, _| {
+                app.quit();
+            })
+        );
+        self.app.set_accels_for_action("app.quit", &["<primary>q"]);
     }
 
     fn setup_signals(&self) {
@@ -26,7 +39,7 @@ impl Application {
             let win = gtk::ApplicationWindow::new(app);
 
             win.set_default_size(320, 240);
-            win.set_title("Roots");
+            win.set_title("Priority");
 
             win.show_all();
         });
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+#[macro_use]
+extern crate glib;
+
+#[macro_use]
+mod utils;
 mod application;
 mod config;
-mod utils;
 
 use application::Application;
 
